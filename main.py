206d344bf2d3885557ee19fc8584f2a33ede455c
W2nd3r <109763425+W2nd3r@users.noreply.github.com>
2024-05-02 21:30:31 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+import os
+
+# get link
+url = input("Enter Link:")
+yt = YouTube(url)
+
+# getting home directory
+download_path = os.path.expanduser('~/Downloads')
+
+# for detailed description of available downloads
+#print(yt.streams.filter(file_extension="mp4"))
+
+# downloading video and where to save
+stream = yt.streams.get_by_itag(22)
+stream.download(download_path)
